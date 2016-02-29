a8a514a733364774360471b6e0784012057d2c4a
Derlei <derlei@unidavi.edu.br>
2016-02-29 20:47:50 -0300

    Corrigido outro bug

--- src/puzzle8/Puzzle8.java
+++ src/puzzle8/Puzzle8.java
@@ -6,50 +6,53 @@ package puzzle8;
  */
 public class Puzzle8 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ListaTabuleiros processados = new ListaTabuleiros();
         ListaTabuleiros abertos = new ListaTabuleiros();
         boolean achou = false;
         int count = 0;
         int qtdMovimentos = 0;
 
-        Tabuleiro inicio = new Tabuleiro(2, 0, 1, 8, 4, 6, 5, 3, 7);
+        Tabuleiro inicio = new Tabuleiro(2, 3, 7, 6, 5, 8, 4, 0, 1);
         Tabuleiro fim = new Tabuleiro(1, 2, 3, 4, 5, 6, 7, 8, 0);
         Tabuleiro resultado = new Tabuleiro();
 
         abertos.add(inicio);
 
+        if (inicio.equals(fim)) {
+            System.out.println("Final igual ao inicio \n" + inicio.toString());
+            System.out.println(String.format("Total de Verificacoes: %s", count));
+            achou = true;
+        }
+
         do {
-            if(inicio.equals(fim)){
-                System.out.println("Final igual ao inicio \n" + inicio.toString());
-                System.out.println(String.format("Total de Verificacoes: %s", count));
-                break;
-            }
-                      
+
             ListaTabuleiros filhos = abertos.get(0).getFilhos();
-            processados.add((Tabuleiro)abertos.get(0));
-            
+            processados.add((Tabuleiro) abertos.get(0));
+
             for (Tabuleiro filho : filhos) {
-                if(!processados.contains(filho)){
+                if (!processados.contains(filho)) {
                     abertos.add(filho);
                     System.out.println(filho.toString());
                     count++;
                 }
-                if(fim.equals(filho)){
+
+                if (fim.equals(filho)) {
                     achou = true;
                     System.out.println("Fim encontrado");
                     System.out.println(fim.toString());
                     System.out.println(String.format("Total de Verificacoes: %s", count));
-                    fim.setPai(abertos.get(0));
-                    resultado = fim;
-                    while(resultado.getPai() != null){
-                        qtdMovimentos ++;
-                        System.out.println("Pai: \n" +resultado.getPai().toString());
+                    filho.setPai(abertos.get(0));
+                    resultado = filho;
+                    while (resultado.getPai() != null) {
+                        qtdMovimentos++;
+                        System.out.println("Pai: \n" + resultado.getPai().toString());
                         resultado = resultado.getPai();
                     }
                     System.out.println("Movimentos: " + qtdMovimentos);
+                    break;
                 }
-                
+
             }
             abertos.remove(0);
         } while (!achou);
